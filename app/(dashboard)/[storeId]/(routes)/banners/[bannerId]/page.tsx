3438943ea2fda8a694fbc10cb9118805b26e6af3
Arief <miftahularifin102@gmail.com>
2025-10-24 16:35:06 +0700

    newagain

--- app/(dashboard)/[storeId]/(routes)/banners/[bannerId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/banners/[bannerId]/page.tsx
@@ -1,24 +1,29 @@
 import db from "@/lib/db";
 import { BannerForm } from "./components/banner-form";
 
-const BannerPage = async ({
-    params
+type Params = {
+  storeId: string;
+  bannerId: string;
+};
+
+export default async function BannerPage({
+  params,
 }: {
+  params: Promise<Params>;
+}) {
+  const { bannerId } = await params;
 
-    params: { bannerId: string }
-}) => {
-    const banner = await db.banner.findUnique({
-        where: {
-            id: params.bannerId
-        }
-    })
-    return (
-        <div className="flex-col">
-            <div className="flex-1 space-y-4 p-8 pt-6">
-            <BannerForm initialData={banner} />
-            </div>
-        </div>
-    );
+  const banner = await db.banner.findUnique({
+    where: {
+      id: bannerId,
+    },
+  });
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <BannerForm initialData={banner} />
+      </div>
+    </div>
+  );
 }
-
-export default BannerPage;
